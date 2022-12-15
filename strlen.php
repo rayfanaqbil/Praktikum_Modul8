76c5ad2c5c6bfd23bbfd0d1517370b856e38d3a2
rayfanaqbil <rayfana09@gmail.com>
2022-12-15 16:07:49 +0700

    phpmodul8

---
+++ strlen.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fungsi : String</title>
+</head>
+<body>
+    <h2>Fungsi : String strlen()</h2>
+    <?php
+    $string-"Pemerograman Web PHP";
+    $jml=strlen($string);
+    echo ("<1>\ "$string\"</1>
+    <br>");
+    echo ("Banyak karakter adalah $jml");
+    ?>
+</body>
+</html>
